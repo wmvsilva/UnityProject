3523faa7320d9ecba01b12c2a7e58b8ae02ade08
wmvsilva <silva.w@husky.neu.edu>
2015-04-11 18:36:00 -0500

    Added Unity Project. Command Line added.
    Command line can echo and tell you when command does not exist.

---
+++ 13thSoleHeaven/Assets/Scripts/CommandScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CommandScript : MonoBehaviour {
+
+	public void giveCommand(string cmd) {
+		string[] cmdArray = cmd.Split (' ');
+		string command = cmdArray[0];
+		List<string> arguments = new List<string> ();
+		for (int i = 1; i < cmdArray.Length; i++) {
+			arguments.Add(cmdArray[i]);
+		}
+		switch (command) {
+		case ("echo"):
+			echo(arguments);
+			break;
+		default:
+			commandNotFound(command);
+			break;
+		}
+	}
+
+	void echo(List<string> args) {
+		string stringArgs = "";
+		foreach (string s in args) {
+			stringArgs = stringArgs + s + " ";
+		}
+		Debug.Log (stringArgs);
+	}
+
+	void commandNotFound(string command) {
+		Debug.Log ("Error- " + command + " command not found");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ 13thSoleHeaven/Assets/Scripts/TextGUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextGUI : MonoBehaviour {
+
+	private string textFieldString = "";
+
+	void OnGUI ()
+	{
+		GUI.SetNextControlName ("CommandLine");
+		Event e = Event.current;
+		if (e.type == EventType.keyDown && e.keyCode == KeyCode.Return && GUI.GetNameOfFocusedControl() == "CommandLine") {
+			if (!textFieldString.Equals("")) {
+			sendCommand(textFieldString);
+			textFieldString = "";
+			}
+		}
+		textFieldString = GUI.TextField (new Rect (0, Screen.height - 25, Screen.width, 25), textFieldString, 25);
+	}
+
+	void sendCommand(string cmd) {
+		transform.parent.FindChild ("CommandController").GetComponent<CommandScript> ().giveCommand (cmd);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
