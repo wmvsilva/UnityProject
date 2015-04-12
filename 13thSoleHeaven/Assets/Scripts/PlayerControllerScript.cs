47ac7631fb522a1a16582613f845cd1208a6dd76
wmvsilva <silva.w@husky.neu.edu>
2015-04-12 14:51:50 -0500

    Added Basic Player Object
    Player object can move around and is represented by a white circle
    sprite

---
+++ 13thSoleHeaven/Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerScript : MonoBehaviour {
+
+	PlayerMovementScript movementScript;
+
+	// Use this for initialization
+	void Start () {
+		movementScript = transform.FindChild ("MovementController").GetComponent<PlayerMovementScript> ();
+		if (movementScript == null) {
+			Debug.LogError("Could not load Player Movement Script");
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movementScript.myUpdate ();
+	}
+
+	void FixedUpdate() {
+		rigidbody2D.velocity = movementScript.myFixedUpdate ();
+	}
+}
---
+++ 13thSoleHeaven/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(50, 50);
+	private Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void myUpdate () {
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+
+		movement = new Vector2 (speed.x * inputX, speed.y * inputY);
+	}
+
+	public Vector2 myFixedUpdate() {
+		return movement;
+	}
+}
