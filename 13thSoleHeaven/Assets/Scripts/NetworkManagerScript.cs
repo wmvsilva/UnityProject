7a3bcd0fddf00afa520f791b42246cee8b8fdcd3
wmvsilva <silva.w@husky.neu.edu>
2015-04-12 19:47:20 -0500

    Added Network Manager. Player can spawn at 0,0,0
    Added Network Manager. Player just spawns at 0,0,0 over the network. The
    player prefab was moved to Prefabs/Resources to do this.

---
+++ 13thSoleHeaven/Assets/Scripts/NetworkManagerScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManagerScript : MonoBehaviour {
+	
+	public PlayerControllerScript player;
+	
+	// Use this for initialization
+	void Start () {
+		Connect();
+	}
+	
+	void Connect() {
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+	
+	void OnGUI() {
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+	
+	void OnJoinedLobby() {
+		Debug.Log("OnJoinedLobby");
+		PhotonNetwork.JoinRandomRoom();
+	}
+	
+	void OnPhotonRandomJoinFailed() {
+		Debug.Log("OnPhotonRandomJoinFailed");
+		PhotonNetwork.CreateRoom(null);
+	}
+	
+	void OnJoinedRoom() {
+		Debug.Log("OnJoinedRoom");
+		SpawnMyPlayer();
+	}
+	
+	void SpawnMyPlayer() {
+		Debug.Log("SpawnMyPlayer");
+		GameObject myPlayer = (GameObject) PhotonNetwork.Instantiate ("Player",
+		                                                              Vector3.zero,
+		                                                              Quaternion.identity, 0);
+		player = myPlayer.GetComponent<PlayerControllerScript> ();
+		if (myPlayer == null) {
+			Debug.Log("myPlayer not found");
+			return;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
