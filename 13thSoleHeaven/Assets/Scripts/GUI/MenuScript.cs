47e6d28aa233e1f693fc7e806fd4d9f072f8a43d
wmvsilva <silva.w@husky.neu.edu>
2015-06-07 21:11:22 -0500

    Added last name and start button to main menu

--- 13thSoleHeaven/Assets/Scripts/GUI/MenuScript.cs
+++ 13thSoleHeaven/Assets/Scripts/GUI/MenuScript.cs
@@ -3,20 +3,19 @@ using System.Collections;
 
 public class MenuScript : MonoBehaviour {
 
-	string textFieldString = "";
+	string firstName = "";
+	string lastName = "";
 
 	public GameObject staticObj;
 
 	void OnGUI(){
-		GUI.SetNextControlName ("CommandLine");
-		Event e = Event.current;
-		if (e.type == EventType.keyDown && e.keyCode == KeyCode.Return && GUI.GetNameOfFocusedControl () == "CommandLine") {
-			if (!textFieldString.Equals ("")) {
-				staticObj.GetComponent<StaticInformation>().setPlayerName(textFieldString);
-				GameObject.DontDestroyOnLoad(staticObj);
-				Application.LoadLevel(1);
-			}
+		GUI.SetNextControlName ("Main");
+		if (GUI.Button (new Rect(Screen.width / 2, Screen.height / 2 + 60, 75, 25), "Start")) { 
+			staticObj.GetComponent<StaticInformation>().setPlayerName(firstName + " " + lastName);
+			GameObject.DontDestroyOnLoad(staticObj);
+			Application.LoadLevel(1);
 		}
-		textFieldString = GUI.TextField (new Rect(Screen.width / 2, Screen.height / 2, 75, 25) , textFieldString, 25);
+		firstName = GUI.TextField (new Rect(Screen.width / 2, Screen.height / 2, 75, 25) , firstName, 25);
+		lastName = GUI.TextField (new Rect(Screen.width / 2, Screen.height / 2 + 30, 75, 25) , lastName, 25);
 	}
 }
