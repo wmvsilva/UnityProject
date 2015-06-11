08ef8866c73e5f1f17cd0434f9f6207abe6884b7
wmvsilva <silva.w@husky.neu.edu>
2015-06-10 21:39:13 -0500

    Added player sprite script and torso sprite

---
+++ 13thSoleHeaven/Assets/Scripts/PlayerSpriteScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpriteScript : MonoBehaviour {
+
+	public Sprite[] torsoSprites;
+	SpriteRenderer torsoRenderer;
+
+	// Use this for initialization
+	void Start () {
+		torsoSprites = Resources.LoadAll<Sprite>("Torso");
+		torsoRenderer = transform.FindChild ("TorsoSprite").GetComponent<SpriteRenderer> ();
+		if (torsoRenderer == null) {
+			Debug.LogError("Torso renderer was null");
+		}
+		torsoRenderer.sprite = torsoSprites [0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
