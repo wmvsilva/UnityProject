a94e4dfc3a5085debfad2af335606b8e95dc0e27
wmvsilva <silva.w@husky.neu.edu>
2015-06-22 22:09:44 -0500

    Updated sprites. Added Head and Arm.

--- 13thSoleHeaven/Assets/Scripts/PlayerSpriteScript.cs
+++ 13thSoleHeaven/Assets/Scripts/PlayerSpriteScript.cs
@@ -4,16 +4,26 @@ using System.Collections;
 public class PlayerSpriteScript : MonoBehaviour {
 
 	public Sprite[] torsoSprites;
+	public Sprite[] headSprites;
+	public Sprite[] leftArmSprites;
 	SpriteRenderer torsoRenderer;
+	SpriteRenderer headRenderer;
+	SpriteRenderer leftArmRenderer;
 
 	// Use this for initialization
 	void Start () {
-		torsoSprites = Resources.LoadAll<Sprite>("Torso");
-		torsoRenderer = transform.FindChild ("TorsoSprite").GetComponent<SpriteRenderer> ();
-		if (torsoRenderer == null) {
-			Debug.LogError("Torso renderer was null");
+		LoadSprites ("TorsoSprites", "TorsoSprite", torsoSprites, torsoRenderer);
+		LoadSprites ("HeadSprites", "HeadSprite", headSprites, headRenderer);
+		LoadSprites ("LeftArmSprites", "LeftArmSprite", leftArmSprites, leftArmRenderer);
+	}
+
+	void LoadSprites(string spriteName, string childName, Sprite[] sprites, SpriteRenderer renderer) {
+		sprites = Resources.LoadAll<Sprite>(spriteName);
+		renderer = transform.FindChild (childName).GetComponent<SpriteRenderer> ();
+		if (renderer == null) {
+			Debug.LogError(childName + " renderer was null");
 		}
-		torsoRenderer.sprite = torsoSprites [0];
+		renderer.sprite = sprites [0];
 	}
 	
 	// Update is called once per frame
