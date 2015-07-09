d32c6d9b7ecffc87ae6e239d9882a1334361d9f7
wmvsilva <silva.w@husky.neu.edu>
2015-07-08 20:21:50 -0500

    Added button on scene for hand swap. No logic yet.

---
+++ 13thSoleHeaven/Assets/Scripts/GUI/GameGUIScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+/**
+ * Script to control all GUI buttons that the player will use to interact with the game.
+ */
+public class GameGUIScript : MonoBehaviour {
+
+	/**
+	 * Swaps the currently selected hand from left to right or vice versa. 
+	 */
+	public void swapHands() {
+		//TODO
+		Debug.Log ("Swapped hands");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
